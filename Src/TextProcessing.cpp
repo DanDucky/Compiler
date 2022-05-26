9ac0b1ceef5975f764bae44ff2c4f08d903a3ce9
DanDucky <holdenwellsbennett@gmail.com>
2022-05-26 10:55:43 -0700

    Added basic goto functionality
    das wat it does

--- Src/TextProcessing.cpp
+++ Src/TextProcessing.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+#include <bitset>
 
 int Move (std::string Args) {
 	return 0;
@@ -9,8 +11,27 @@ int Clear (std::string Args) {
 	return 0;
 }
 
-int Goto (std::string Args) {
-	return 0;
+std::string Goto (std::string Args, std::string Instruction) {
+	std::string InstructionParams = Args.substr(1,4);
+	if (InstructionParams == "STOR") {
+		Instruction = Instruction + "1";
+	} else if (InstructionParams == "READ") {
+		Instruction = Instruction + "0";
+	} else {
+		std::cout << "Error in instruction parameter 1: \"" << InstructionParams << "\" on line 1\nExiting...\n";
+		exit(EXIT_FAILURE);
+	}
+	InstructionParams = Args.substr(5);
+	Instruction = Instruction + std::bitset<3>(stoi(InstructionParams)).to_string();
+	/*
+	InstructionParams = Args.substr(6);
+	if (InstructionParams == " ") {
+		InstructionParams = Args.substr(5);
+	} else {
+		InstructionParams = Args.substr(5,6);
+	}
+	*/
+	return Instruction;
 }
 
 int Declare (std::string Args) {
@@ -19,14 +40,18 @@ int Declare (std::string Args) {
 
 int main () {
 	std::string Line;
-	std::cin >> Line;
+	std::getline(std::cin, Line);
+	//TODO create variable for substr instead of recalling function every time (also maybe use switch with pointers?
 	if (Line.substr(0,4) == "GOTO") {
-		std::cout << Goto(Line.substr(5,Line.length()));
+		std::cout << Goto(Line.substr(4,Line.length()), "00");
 	} else if (Line.substr(0,4) == "MOVE") {
 		std::cout << Move(Line.substr(5,Line.length()));
 	} else if (Line.substr(0,4) == "CLER") {
 		std::cout << Clear(Line.substr(5,Line.length()));
 	} else if (Line.substr(0,4) == "DECL") {
 		std::cout << Declare(Line.substr(5,Line.length()));
+	} else {
+		std::cout << "Error in instruction on line 1\nExiting...\n";
+		exit(EXIT_FAILURE);
 	}
 }
