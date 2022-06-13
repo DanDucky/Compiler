3d1e4d4055fa458fe0362ed22e6037c5cf9b290f
DanDucky <holdenwellsbennett@gmail.com>
2022-06-13 16:38:08 -0700

    v1.0
    Full functionality working, QoL features to be added possibly in the future

--- Src/IO.cpp
+++ Src/IO.cpp
@@ -1,19 +1,55 @@
 #include "TextProcessing.hpp"
+#include <fstream>
+#include <chrono>
+using namespace std::chrono;
 
-int main () {
-	std::string Line;
-	std::getline(std::cin, Line);
-	//TODO create variable for substr instead of recalling function every time (also maybe use switch)
-	if (Line.substr(0,4) == "GOTO") {
-		std::cout << Goto(Line.substr(4,Line.length()), "10") << "0000" << std::endl;
-	} else if (Line.substr(0,4) == "MOVE") {
-		std::cout << Move(Line.substr(5,Line.length()), "00") << std::endl;
-	} else if (Line.substr(0,4) == "CLER") {
-		std::cout << Clear(Line.substr(5,Line.length()), "01") << std::endl;
-	} else if (Line.substr(0,4) == "DECL") {
-		std::cout << Declare(Line.substr(5,Line.length()), "11") << std::endl;
+std::string InstructionCall (std::string Line, int LineNum) {
+	std::string Substr = Line.substr(0,4);
+	std::string Instruction;
+	if (Substr == "GOTO") {
+		Instruction = Goto(Line.substr(4,Line.length()), "10") + "0000";
+	} else if (Substr == "MOVE") {
+		Instruction = Move(Line.substr(5,Line.length()), "00");
+	} else if (Substr == "CLER") {
+		Instruction = Clear(Line.substr(5,Line.length()), "01");
+	} else if (Substr == "DECL") {
+		Instruction = Declare(Line.substr(5,Line.length()), "11");
 	} else {
-		std::cout << "Error in instruction on line 1\nExiting...\n";
+		std::cout << "\033[1;31mError\033[0m in instruction on line " << LineNum << "\nExiting...\n";
 		exit(EXIT_FAILURE);
 	}
+	return Instruction;
+}
+int main (int argc, char** argv) {
+    std::ifstream Source;
+    Source.open(argv[1]);
+    std::string Line;
+    int LineNum = 0;
+    std::ofstream Binary("Out.dab");
+    std::string LineOut;
+    int ArrowLength;
+    int Arrow = 0;
+    if (Source.is_open()) {
+    	while (std::getline (Source, Line)) {
+    		auto start = high_resolution_clock::now();
+    		LineNum++;
+    		LineOut = InstructionCall(Line, LineNum);
+    		Binary << LineOut << "\n";
+    		std::cout << Line << " ";
+    		ArrowLength = 22 - Line.length();
+    		while (Arrow < ArrowLength) {
+    			std::cout << "-";
+    			Arrow++;
+    		}
+    		Arrow = 0;
+    		auto stop = high_resolution_clock::now();
+    		auto Duration = duration_cast<microseconds>(stop - start);
+    		std::cout << "> " << LineOut << " | Line " << LineNum << " \033[1;32mcompleted\033[0m in " << Duration.count() << " microseconds \n";
+    	}
+    	std::cout << "Successfully wrote " << LineNum << " line(s) to Out.dab\n";
+    } else {
+    std::cout << "Error in opening source file, file does not exist\nExiting...\n";
+    exit(EXIT_FAILURE);
+    }
+    Binary.close();
 }
